bc846ab7f1b7a0cf4cf36b9d27f454b6b88d4fc4
stuart moore <stuart@stuart-moore.com>
2020-03-17 16:56:59 +0000

    schema mvp done, starting roles

--- dbaSecurityScan/Tests/Schema.Tests.ps1
+++ dbaSecurityScan/Tests/Schema.Tests.ps1
@@ -1,7 +1,7 @@
 param (
     [object]$config,
     [Object]$SqlInstance,
-    [SecureString]$SqlCredential,
+    [PSCredential]$SqlCredential,
     [String]$Database
 )
 
@@ -19,10 +19,24 @@ $sqlSchema = "
             sys.schemas ss inner join sys.database_principals sdp 
                 on ss.principal_id=sdp.principal_id
 "
-$dbSchema = Invoke-DbaQuery @connectionSplat -Query $sqlSchema
-Foreach ($schema in $config.schema) {
-    It "Schema $($schema.schemaname)should exist and be owned by $($schema.owner) in database" {
-        ($dbSchema | Where-Object ($_.schemaName -eq $schema.schemaname -and $_.owner -eq $schema.owner)).count | Should -Be 1
-    }
+$dbSchema = Invoke-DbaQuery -SqlInstance $SqlInstance -sqlcredential $sqlcredential -database "schema1" -Query $sqlSchema 
 
+Foreach ($schema in $config.schemas) {
+    Describe "Checking schema $($schema.schemaname)" {
+        It "Schema $($schema.schemaname) should exist and be owned by $($schema.owner)" {
+            ($dbSchema | Where-Object {$_.schemaName -eq $schema.schemaname -and $_.owner -eq $schema.owner}).count | Should -Be 1
+        }
+        $checkSql = "select name, type_desc from sys.all_objects where schema_id=SCHEMA_ID('$($schema.schemaname)')"
+        $objectsSchema = Invoke-DbaQuery -SqlInstance $SqlInstance -sqlcredential $sqlcredential -database "schema1" -Query $checkSql 
+            
+        It "Schema $($schema.schemaname) should contain $($schema.objects.count) Objects" {
+            $objectsSchema.count | Should -Be $schema.objects.count -Because "The schema should only contain the specified objects"
+        }
+
+        Foreach ($object in  $schema.objects) {
+            It "$($schema.schemaname) should contain $($object.object)"{
+                ($objectsSchema | Where-Object {$_.name -eq $object.Object }).count | Should -Be 1
+            }
+        }
+    }
 }
