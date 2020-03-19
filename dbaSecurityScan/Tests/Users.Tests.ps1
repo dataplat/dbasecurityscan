8c14fc73127f08ca12140f070eb54e064c3fd56c
Stuart Moore <stuart@stuart-moore.com>
2020-03-19 13:47:07 +0000

    test fix

--- dbaSecurityScan/Tests/Users.Tests.ps1
+++ dbaSecurityScan/Tests/Users.Tests.ps1
@@ -10,18 +10,18 @@ $filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 $dbUsers = Get-DbaDbUser -SqlInstance $SqlInstance -SqlCredential $SqlCredential -Database $database
 $dbUserRoles = Get-DbaDbRoleMember -SqlInstance $sqlinstance -SqlCredential $SqlCredential -Database $database -IncludeSystemUser
 $dbRoles = Get-DbaDbRole -SqlInstance $sqlinstance -SqlCredential $SqlCredential -Database $database 
-Foreach ($case in $config) {
+Foreach ($case in $config.users) {
     Describe "Checking $($case.username)" {
         It "$($case.username) should exist" {
             $case.username | Should -BeIn $dbUsers.Name -Because "User Should exist"
         }
-
-        Foreach ($role in $case.roles){
-            It "$($case.username) should be a member of $role " {
-                $role | Should -BeIn $dbUserRoles.Role
+        if ($case.roles.count -gt 0){
+            Foreach ($role in $case.roles){
+                It "$($case.username) should be a member of $role " {
+                    $role | Should -BeIn $dbUserRoles.Role
+                }
             }
         }
-
         $testRoles = $dbUserRoles | where-Object {$case.UserName -eq $_.UserName}
         ForEach ($role in $testRoles){
             It "$($case.username) Should be in $($role.Role)" {
@@ -29,22 +29,22 @@ Foreach ($case in $config) {
             }
         }
 
-        # if ($_.Permissions.Count -ge 1) {
+        if ($case.Permissions.Count -ge 1) {
             $testPermissions =  Get-DbaUserPermission -SqlInstance $SqlInstance -SqlCredential $SqlCredential -Database $database
             # Go through to check the specified permissions are there
             Foreach($permission in $case.Permissions){
                 It "Should have assigned $($case.userName) $($permission.permission) on $($permission.securable)" {
-                    ($testPermissions | Where-Object {$_.Grantee -eq $case.username -and $_.Securable -eq $permission.securable -and $_.permission -eq $permission.permission}).Count | Should -Be 1
+                    ($testPermissions | Where-Object {$_.Grantee -eq $case.username -and $_.Securable -eq $permission.securable -and $_.permission -eq $permission.permission} | Measure-Object).count | Should -Be 1
                 }
 
             }
             # Go through again to make sure no other permissions are there
-            # Foreach ($permission in $testPermissions){
+            #Foreach ($permission in $testPermissions){
                 It "Should not have assigned $($case.userName) $($permission.permission) on $($permission.securable)" {
-                    ($testPermissions | Where-Object {$_.Grantee -eq $case.username -and $_.Securable -notin ($case.permissions.securable) -and $_.permission -notin ($case.permissions.permission)}).Count | Should -Be 0 -Because "Should only have defined permissions"
+                    ($testPermissions | Where-Object {$_.Grantee -eq $case.username -and $_.Securable -notin ($case.permissions.securable) -and $_.permission -notin ($case.permissions.permission)} | Measure-Object).Count | Should -Be 0 -Because "Should only have defined permissions"
                 }
-                $testPermissions | Where-Object {$_.Grantee -eq $case.username -and $_.Securable -notin ($case.permissions.securable) -and $_.permission -notin ($case.permissions.permission)}
-            # }
-        # }
+               $testPermissions | Where-Object {$_.Grantee -eq $case.username -and $_.Securable -notin ($case.permissions.securable) -and $_.permission -notin ($case.permissions.permission)}
+           # }
+        }
     }
 }
