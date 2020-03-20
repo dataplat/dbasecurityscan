f3e53756de61a17f3b73a0ebdb8ba52e01303e20
stuart moore <stuart@stuart-moore.com>
2020-03-20 17:19:04 +0000

    add sql

--- tests/Get-DssConfig.Tests.ps1
+++ tests/Get-DssConfig.Tests.ps1
@@ -2,6 +2,15 @@ $commandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 . "$PSScriptRoot\constants.ps1"
 Describe "Unit tests for $commandName" {
     It "$commandName Should Exist" {
-        (Get-Command -Module dbaSecurityScan | Where-Object {$_.Name -eq $commandName}).count | Should -Be 0
+        (Get-Command -Module dbaSecurityScan | Where-Object {$_.Name -eq $commandName}).count | Should -Be 1
+    }
+}
+(& sqlcmd -S localhost\sql2017 -U -b -i "$PSScriptroot\tests\scenarios\normal1\normal1.sql" -d "master")
+$sqlinstance = 'localhost\sql2017'
+Describe "Integration tests for $commandName" {
+    $outfile = 'c:\temp\Userconfig.json'
+    Get-DssConfig -SqlInstance $sqlinstance  -Database Normal1 -ConfigPath $outfile -UserConfig
+    It "Should get a UserConfig" {
+        Test-Path $outfile | Should -Be $true
     }
 }
