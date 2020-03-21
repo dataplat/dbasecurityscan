cf542d24dab67e00189d0d85cc8167a51e8ede57
stuart moore <stuart@stuart-moore.com>
2020-03-21 10:41:33 +0000

    appv18

--- tests/Get-DssConfig.Tests.ps1
+++ tests/Get-DssConfig.Tests.ps1
@@ -5,12 +5,15 @@ Describe "Unit tests for $commandName" {
         (Get-Command -Module dbaSecurityScan | Where-Object {$_.Name -eq $commandName}).count | Should -Be 1
     }
 }
-$sqlInstance = '(local)\sql2017'
-(& sqlcmd -S "$sqlInstance" -U "sa" -P "Password12!" -b -i "$PSScriptroot\scenarios\normal1\normal1.sql" -d "master")
+# $sqlInstance = '(local)\sql2017'
+# (& sqlcmd -S "$sqlInstance" -U "sa" -P "Password12!" -b -i "$PSScriptroot\scenarios\normal1\normal1.sql" -d "master")
 
 Describe "Integration tests for $commandName" {
-    $outfile = "$PSScriptRoot\scenarios\normal1\test.json"
+    $outfile = "c:\github\dbaSecurityScan\tests\scenarios\normal1\test.json"
     $config = Get-DssConfig -ConfigPath $outfile 
+    It "File Config Should Exist" {
+        Test-Path $outfile | Should -Be $true
+    }
     It "Should get a Config" {
         $config.Count | Should -Be 1
     }
