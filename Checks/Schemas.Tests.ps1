3b950226f829880116bb12e94ffa5dc353dcb0b8
stuart moore <stuart@stuart-moore.com>
2020-03-22 14:10:21 +0000

    fix ps5 issue

--- Checks/Schemas.Tests.ps1
+++ Checks/Schemas.Tests.ps1
@@ -5,12 +5,6 @@ param (
     [String]$Database
 )
 
-$connectionSplat = @{
-            SqlInstance  = $SqlInstance
-            SqlCredential = $SqlCredential
-            Database      = $Database
-}
-
 # FIXME: Potential dbatools command to migrate across. Get Schema details
 $sqlSchema = "
         select 
@@ -25,7 +19,7 @@ $dbSchema = Invoke-DbaQuery -SqlInstance $SqlInstance -sqlcredential $sqlcredent
 Foreach ($schema in $config.schemas) {
     Describe "Checking schema $($schema.schemaname)" {
         It "Schema $($schema.schemaname) should exist and be owned by $($schema.owner)" {
-            ($dbSchema | Where-Object {$_.schemaName -eq $schema.schemaname -and $_.owner -eq $schema.owner}).count | Should -Be 1
+            ($dbSchema | Where-Object {$_.schemaName -eq $schema.schemaname -and $_.owner -eq $schema.owner} | Measure-Object).count | Should -Be 1
         }
         $checkSql = "select name, type_desc from sys.all_objects where schema_id=SCHEMA_ID('$($schema.schemaname)') and is_ms_shipped=0"
         $objectsSchema = Invoke-DbaQuery -SqlInstance $SqlInstance -sqlcredential $sqlcredential -database "schema1" -Query $checkSql 
@@ -36,7 +30,7 @@ Foreach ($schema in $config.schemas) {
 
         Foreach ($object in  $schema.objects) {
             It "$($schema.schemaname) should contain $($object.object)"{
-                ($objectsSchema | Where-Object {$_.name -eq $object.Object }).count | Should -Be 1
+                ($objectsSchema | Where-Object {$_.name -eq $object.Object } | Measure-Object).count | Should -Be 1
             }
         }
     }
