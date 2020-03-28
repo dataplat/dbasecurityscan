b8e96d0c305fce6d7365b4c5cfcad7ca3b6f1363
stuart moore <stuart@stuart-moore.com>
2020-03-28 12:41:35 +0000

    can has linux? 3

--- tests/constants.ps1
+++ tests/constants.ps1
@@ -3,7 +3,13 @@ if (Test-Path $HOME\dbaSecurityScan-constants.ps1) {
     . $HOME\dbaSecurityScan-constants.ps1
 } else {
     $script:appvSqlInstance = "localhost\sql2017"
-    $script:appvModuleroot = 'c:\github\dbaSecurityScan'
+    if ($ENV:APPVEYOR -and $IsLinux) { 
+        $script:appvModuleroot = 
+    } elseif ($ENV:APPVEYOR -and $IsWindows)  {
+        $script:appvModuleroot = '/home/appveyor/projects/dbasecurityscan/'
+    } else {
+        $script:appvModuleroot = 'C:\projects\dbasecurityscan\'
+    }
     $script:appvPassword = ConvertTo-SecureString 'Password12!' -AsPlainText -Force
     $script:appvSqlCredential = New-Object System.Management.Automation.PSCredential ('sa', $appvPassword)
     $script:IgnoreSQLCMD = $false
