02d2b948b2310f59bdbd9bfad12870aae2254204
stuart moore <stuart@stuart-moore.com>
2020-04-04 14:47:56 +0100

    windows tests

--- Tests/constants.ps1
+++ Tests/constants.ps1
@@ -4,12 +4,9 @@ if (Test-Path $HOME\dbaSecurityScan-constants.ps1) {
 } else {
     if ($ENV:APPVEYOR -and $IsLinux) { 
         $script:appvSqlInstance = "localhostSQL2019"
-    } else {
-        $script:appvSqlInstance = "localhost\SQL2019"
-    }
-    if ($ENV:APPVEYOR -and $IsLinux) { 
         $script:appvModuleroot = '/home/appveyor/projects/dbasecurityscan/'
-    } elseif ($ENV:APPVEYOR -and $IsWindows)  {
+    } elseif ($ENV:APPVEYOR) {
+        $script:appvSqlInstance = "localhost\SQL2019"
         $script:appvModuleroot = 'C:\projects\dbasecurityscan\'
     } else {
         $script:appvModuleroot = 'c:\github\dbasecurityscan'
