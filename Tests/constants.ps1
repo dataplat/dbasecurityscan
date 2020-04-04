308ea670223f97359926e07f13c7da0df9d3bf39
stuart moore <stuart@stuart-moore.com>
2020-04-04 15:59:41 +0100

    []

--- Tests/constants.ps1
+++ Tests/constants.ps1
@@ -8,10 +8,10 @@ if (Test-Path $HOME\dbaSecurityScan-constants.ps1) {
         $script:appvModuleroot = 'C:\projects\dbasecurityscan\'
     } elseif ($IsLinux) {
         $script:appvSqlInstance = "localhost:1433\SQL2019"
-    } else[
+    } else {
         $script:appvModuleroot = 'c:\github\dbasecurityscan'
         $script:appvSqlInstance = "localhost\SQL2019"
-    ]
+    }
 
     $script:appvPassword = ConvertTo-SecureString 'P@$sw0rdling' -AsPlainText -Force
     $script:appvSqlCredential = New-Object System.Management.Automation.PSCredential ('sa', $appvPassword)
