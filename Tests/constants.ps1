f6266305e99aa225618c31dab92763eb04af95cb
stuart moore <stuart@stuart-moore.com>
2020-04-04 14:59:36 +0100

    fixing instance name

--- Tests/constants.ps1
+++ Tests/constants.ps1
@@ -3,14 +3,13 @@ if (Test-Path $HOME\dbaSecurityScan-constants.ps1) {
     . $HOME\dbaSecurityScan-constants.ps1
 } else {
     if ($ENV:APPVEYOR -and $IsLinux) { 
-        $script:appvSqlInstance = "localhostSQL2019"
         $script:appvModuleroot = '/home/appveyor/projects/dbasecurityscan/'
     } elseif ($ENV:APPVEYOR) {
-        $script:appvSqlInstance = "localhost\SQL2019"
         $script:appvModuleroot = 'C:\projects\dbasecurityscan\'
     } else {
         $script:appvModuleroot = 'c:\github\dbasecurityscan'
     }
+    $script:appvSqlInstance = "localhost\SQL2019"
     $script:appvPassword = ConvertTo-SecureString 'P@ssword!!' -AsPlainText -Force
     $script:appvSqlCredential = New-Object System.Management.Automation.PSCredential ('sa', $appvPassword)
     $script:IgnoreSQLCMD = $true
